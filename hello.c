c2c37614d67c0fd4df95990431dd11bc7a9ee3cd
MathewPagan <55605022+MathewPagan@users.noreply.github.com>
2019-09-20 23:06:15 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Mathew Pagan and Modified by __________
+ * Date: 2019/09/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Mathew Pagan\n");
+
+  return 0;
+}
